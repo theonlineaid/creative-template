7c32d202558e69fd8f00ccaae526077f532c89b0
nkomorfaruk <nkomorfaruk@gmail.com>
2019-01-25 12:05:28 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+
+    $('.basic-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+
+    });
+
+    $('.testmonil').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+    $('.branding-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+  });
